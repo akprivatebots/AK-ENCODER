2bd4b3b69c85bf0e8c92b68e335a2352ad41ac67
akprivatebots <90455659+akprivatebots@users.noreply.github.com>
2022-03-29 13:40:59 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -1,15 +1,16 @@
 #Don't be a thief by stealing other's Hardwork, it took time and effort to make this repo.
 #Respect my work by not making any changes here.
+#Sorry bro I have to.😑😓
 
-START_TEXT = "Send me any file to begin."
+START_TEXT = "Send me any file to begin the process."
 
-FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
+FORCE_SUB_TEXT = "In order to use this bot, you've to join my this channel."
 
-CHANNEL_LINK = "https://t.me/DroneBots"
+CHANNEL_LINK = "https://t.me/MOVIEFINATICSS"
 
-SUPPORT_LINK = "https://t.me/TeamDrone"
+SUPPORT_LINK = "https://t.me/AKLEECHZONE"
 
-info_text = "This bot is developed by @MaheshChauhan\n\nWritten in python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.4"   
+info_text = "This bot is developed by @AK_NH4\n\nWritten in python library TELETHON.\n\nV1.4"   
 
 help_text = """**v1.4**
 
@@ -29,13 +30,13 @@ help_text = """**v1.4**
 
 source_text = "**Deploy your own bot**"
 
-DEV = "https://t.me/MaheshChauhan"
+DEV = "https://t.me/AK_NH4"
 
 spam_notice = "This bot is hosted on heroku, and hence can just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
 
 JPG = "LOCAL/video_convertor.jpg"
 
-JPG0 = 'https://telegra.ph/file/d98c559b56ef884ef3bad.jpg'
+JPG0 = 'https://telegra.ph/file/fb66adf7d5f9d34791535.jpg'
 
 JPG2 = "LOCAL/20211215_165751.jpg"
 
