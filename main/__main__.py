3d1354e1c573d9fdeb5b8570cf3868185a02c016
akprivatebots <90455659+akprivatebots@users.noreply.github.com>
2022-03-29 15:09:11 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -16,7 +16,7 @@ for name in files:
         load_plugins(plugin_name.replace(".py", ""))
         
 print("Successfully deployed!")
-print("@MaheshChauhan • @DroneBots")
+print("Just hope for not to see any bugs!")
 
 if __name__ == "__main__":
     Drone.run_until_disconnected()
